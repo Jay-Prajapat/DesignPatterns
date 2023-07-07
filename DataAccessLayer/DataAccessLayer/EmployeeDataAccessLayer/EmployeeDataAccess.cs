2da0d4c1c5d990d99b3ea90acb2015d057b01211
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-07-07 11:38:40 +0530

    Completed Practical 22.
    Implemented singleton design pattern.
    Created class library for Data assess layer.
    Implemented one service class for all sql operation.
    Created one logger class with logs the messages.
    Created log class instance with singleton.

---
+++ DataAccessLayer/DataAccessLayer/EmployeeDataAccessLayer/EmployeeDataAccess.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EmployeeDataAccessLayer
+{
+    public sealed class EmployeeDataAccess
+    {
+        public static EmployeeDBContext _dbContext;
+        public static Logger _logger;
+        private static readonly object _lock = new object();
+
+        private EmployeeDataAccess() { }
+
+        public static EmployeeDBContext GetInstance()
+        {
+            if (_dbContext == null)
+            {
+                lock (_lock)
+                {
+                    if (_dbContext == null)
+                    {
+                        _dbContext = new EmployeeDBContext();
+                        return _dbContext;
+                    }
+                }
+            }
+            return _dbContext;
+        }
+        public static Logger GetLogger()
+        {
+            if (_logger == null)
+            {
+                lock (_lock)
+                {
+                    if( _logger == null)
+                    {
+                        _logger = new Logger();
+                        return _logger;
+                    }
+                }
+            }
+            return _logger;
+        }
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"[a-zA-Z ]+$", ErrorMessage = "Name contains only alphabets.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Salary { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        //[Range(typeof(DateTime), "1-1-2002", "06-07-2023", ErrorMessage = "Enter valid date.")]
+        public DateTime JoiningDate { get; set; }
+        public bool Status { get; set; }
+        
+    }
+}
